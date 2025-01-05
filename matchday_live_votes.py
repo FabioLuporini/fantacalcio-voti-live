f95e6fb1b503e14ff332316b15c2cbd5d6058fc6
loop <fantagag@gmail.com>
2025-01-05 16:02:50 +0100

    debug

--- matchday_live_votes.py
+++ matchday_live_votes.py
@@ -662,6 +662,7 @@ if __name__ == "__main__":
         for m in titolari_panchinari:
             for name, vote in list(m.items()):
                 if any(i.startswith(squadre[name]) for i in unplayed):
+                    print("player unplayed gets 6", m[name])
                     m[name] = 6  # S.V.
             print(m)
         print("****")
