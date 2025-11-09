00d5620dd49c1a641c6b22d7822f8e96f285fe1d
loop <fantagag@gmail.com>
2025-11-09 11:47:42 +0100

    debug

--- matchday.py
+++ matchday.py
@@ -329,10 +329,13 @@ if __name__ == "__main__":
     output = ""
     for team, (titolari, panchinari) in fantasquadre.items():
         output += f"Team {team};"
+        output += f"{titolari}\n"
+        output += f"{panchinari}\n"
         try:
             calc_fantasquadra(titolari, panchinari, ruoli)
         except Exception as e:
             output += f"Exception: {str(e)}\n"
+        output += f"----------"
     print(output)
 
     output = {team: calc_fantasquadra(titolari, panchinari, ruoli)
